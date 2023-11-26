26fd2c897f5f64b3032d938f81307cbe48f4d57c
Ala <ala@Alas-MacBook-Pro.local>
2023-11-26 19:22:42 +0200

    did exercise

---
+++ e05/e05.js
@@ -0,0 +1,11 @@
+/*
+1. Use an arrow function to create a function that returns the square of its argument.
+2. Use an arrow function to create a function that takes two arguments and returns their sum.
+*/
+
+
+let myFunction = (a) => a * a;
+let myFunction2 = (a, b) => a + b;
+
+console.log(myFunction(2));
+console.log(myFunction2(1, 2));
