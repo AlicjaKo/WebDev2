857040fd1d9bd4edb16f993dfce5475c3937ed18
Ala <ala@Alas-MacBook-Pro.local>
2023-11-26 16:52:18 +0200

    did exercise

---
+++ e01/e01.js
@@ -0,0 +1,29 @@
+//Exercise 1.
+
+
+let num = 42;
+console.log("num is a " + typeof num);
+
+let str = "Hello word";
+console.log("str is a " + typeof str);
+
+let bool = true;
+console.log("bool is a " + typeof bool);
+
+let nullVar = null;
+console.log("nullVar is an " + typeof nullVar);
+
+let undefiedVar;
+console.log("undefiedVar is " + typeof undefiedVar);
+
+let sym = Symbol("p");
+console.log("sym is a " + typeof sym);
+
+let obj = {
+    key1: "value1",
+    key2: "value2"
+}
+
+console.log("obj is an " + typeof obj);
+
+//////////////////////////////////////
