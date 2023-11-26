4860f03ef512a54783305e8cb0e605c08ba6fca3
Ala <ala@Alas-MacBook-Pro.local>
2023-11-26 17:12:18 +0200

    did exercise

---
+++ e02/e02.js
@@ -0,0 +1,21 @@
+//exercise 2
+
+
+for (let i = 0; i<10; i++) {
+    console.log(i+1);
+}
+for (let i = 10; i>0; i--) {
+    console.log(i);
+}
+
+let a = 0;
+while (a<10) {
+    console.log(a+1);
+    a++
+}
+
+let b = 10;
+while (b>0) {
+    console.log(b);
+    b--
+}
