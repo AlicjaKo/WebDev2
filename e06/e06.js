0442b5bcf3c6484eea17c82ade2b16b66cd1d5e1
Ala <ala@Alas-MacBook-Pro.local>
2023-11-26 19:28:38 +0200

    did exercise

---
+++ e06/e06.js
@@ -0,0 +1,7 @@
+i=99;
+while (i>0) {
+    console.log(`${i} bottles of beer on the wall, ${i} bottles of beer.\nTake one down and pass it around, ${i-1} bottles of beer on the wall.`);
+    i--;
+}
+
+console.log("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.");
